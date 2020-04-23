58add27873517fae63a98e46485ba1bdd557fbcf
rodrigooliveira19 <oliveirarodrigo19@yahoo.com.br>
2020-04-23 19:51:09 -0300

    Criacao de classes do dominio da solucao

---
+++ ProAgil.Domain/Evento.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+namespace ProAgil.Domain
+{
+    public class Evento
+    {
+        public int id { get; set; }
+        public string Local { get; set; }
+        public string DateTime { get; set; }
+        public string Tema { get; set; }
+        public int QtdPessoas { get; set; }
+        public string ImageURL { get; set; }
+        public string telefone { get; set; }
+        public string email { get; set; }
+        public List<Lote> Lotes { get; set; }
+        public List<RedeSocial> RedesSocials { get; set; }
+        public List<PalestranteEvento> PalestrantesEvento { get; set; }
+        
+    }
+}
